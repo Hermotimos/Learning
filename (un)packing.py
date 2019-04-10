7fe9ddb9b4ad510bb8af826eb1ea7d95fb4f2277
Lukasz <lukas.kozicki@gmail.com>
2019-04-10 16:58:00 +0200

    new learning file

--- (un)packing.py
+++ (un)packing.py
@@ -19,8 +19,13 @@ print(dict_2)
 print(*dict_2)
 print(*dict_2.values())
 
+
 print('##########################################################################')
 print('UNPACKING FOR FUNCTIONS')
+tuple_1 = [1, 2, 3, 4]
+list_1 = (1, 2, 3, 4)
+dict_1 = {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+dict_2 = {'a': 'orange', 'b': 'apple', 'c': 'kiwi', 'd': 'banana'}
 
 
 def forfun(a, b, c, d):
@@ -36,12 +41,17 @@ print('**dict accesses values - but they need to be str to be printed out withou
 # forfun(**dict_1)  # - generates error: TypeError: forfun() keywords must be strings
 forfun(**dict_2)
 
+
 print('##########################################################################')
 print('PACKING WITH FUNCTIONS - arbitrary number of args')
-
+tuple_1 = [1, 2, 3, 4]
+list_1 = (1, 2, 3, 4)
+dict_1 = {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+dict_2 = {'a': 'orange', 'b': 'apple', 'c': 'kiwi', 'd': 'banana'}
 
 def sumup(*args):
-    """ Function call packs unknown number of arguments into a tuple, thus enabling us to iterate over it """
+    """ Function call packs unknown number of arguments into a tuple,
+    thus enabling us to iterate over it """
     summ = 0
     for a in args:
         summ += a
@@ -52,9 +62,9 @@ print(sumup(*tuple_1))
 print(sumup(*dict_1))
 print(sumup(*tuple_1, *list_1, *dict_1))
 
+
 print('##########################################################################')
 print('PACKING WITH FUNCTIONS - arbitrary number of kwargs')
-
 
 
 def make_index(**kwargs):
@@ -67,6 +77,5 @@ def make_index(**kwargs):
 dict_for_indexing = {'Malinowski, Bronislaw': ('3', '5', '17', '167'),
                      'Levi-Strauss, Claude': ('4', '9', '89', '134'),
                      'Geertz, Clifford': ('3', '5', '78', '89')}
-
 print(make_index(**dict_for_indexing))
 
