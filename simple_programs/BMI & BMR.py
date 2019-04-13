bb01eee8f8aada30f48021afa8fe7462fcb13f51
Lukasz <lukas.kozicki@gmail.com>
2019-04-13 12:38:13 +0200

    program rearranged in functions

--- simple_programs/BMI & BMR.py
+++ simple_programs/BMI & BMR.py
@@ -7,45 +7,64 @@
 """
 
 
-# Calculating basal metabolic rate (BMR)
-sex_mod = 0
-sex = input("Enter 'M' for male or 'F' for female\n")
+def calculate_bmr():
 
-if sex == "M":
-    sex_mod = 5
-elif sex == 'F':
-    sex_mod = -161
-else:
-    print("Wrong value entered")
+    def ask_sex(): return input("Enter 'M' for male or 'F' for female\n")
+    def ask_weight(): return float(input("Enter your weight in kilograms\n"))
+    def ask_height(): return float(input("Enter your height in centimeters\n"))
+    def ask_age(): return float(input("Enter your age\n"))
 
-weight = float(input("Enter your weight in kilograms\n"))
-height = float(input("Enter your height in centimeters\n"))
-age = float(input("Enter your age\n"))
+    def calculate_sex_mod(genetic_sex):
+        mod = 0
+        if genetic_sex == "M":
+            mod = 5
+        elif genetic_sex == 'F':
+            mod = -161
+        return mod
 
-bmr = 10*weight + 6.25*height - 5*age + sex_mod
+    sex = ask_sex()
+    weight = ask_weight()
+    height = ask_height()
+    age = ask_age()
+    sex_mod = calculate_sex_mod(sex)
 
-# Calculating lifestyle modificator:
-life_style = int(input("What is your lifestyle?\n"
-                   "1 - Sedentary work / no physical activity\n"
-                   "2 - Nonphysical work / little physical activity\n"
-                   "3 - Light physical work / regular exercise 3-4x/week\n"
-                   "4 - Physical work / regular exercise min. 5x/week\n"
-                   "5 - Hard physical work / regular exercise 7x/week\n"))
-life_style_mod = 0
-if life_style == 1:
-    life_style_mod = 1.2
-elif life_style == 2:
-    life_style_mod = 1.4
-elif life_style == 3:
-    life_style_mod = 1.6
-elif life_style == 4:
-    life_style_mod = 1.8
-elif life_style == 5:
-    life_style_mod = 2.0
-else:
-    print("Wrong value entered")
-
-print("\nRESULT:\nYour basal metabolic rate (BMR) = 'no activity' mode : ", bmr, "kcal")
-print("Caloric needs for your lifestyle: ", bmr * life_style_mod, "kcal")
+    bmr = 10 * weight + 6.25 * height - 5 * age + sex_mod
+    return bmr
 
 
+def calculate_lifestyle_mod():
+    def ask_lifestyle():
+        life_style_id = int(input("What is your lifestyle?\n"
+                                  "1 - Sedentary work / no physical activity\n"
+                                  "2 - Nonphysical work / little physical activity\n"
+                                  "3 - Light physical work / regular exercise 3-4x/week\n"
+                                  "4 - Physical work / regular exercise min. 5x/week\n"
+                                  "5 - Hard physical work / regular exercise 7x/week\n"))
+        return life_style_id
+
+    lifestyle_id = ask_lifestyle()
+    life_style_mod = 1
+    if lifestyle_id == 1:
+        life_style_mod = 1.2
+    elif lifestyle_id == 2:
+        life_style_mod = 1.4
+    elif lifestyle_id == 3:
+        life_style_mod = 1.6
+    elif lifestyle_id == 4:
+        life_style_mod = 1.8
+    elif lifestyle_id == 5:
+        life_style_mod = 2.0
+    return life_style_mod
+
+
+def calculate_caloric_needs(bmr):
+    mod = calculate_lifestyle_mod()
+    caloric_needs = bmr * mod
+    return caloric_needs
+
+
+def main():
+    bmr = calculate_bmr()
+    caloric_needs = calculate_caloric_needs(bmr)
+    print("\nRESULT:\nYour basal metabolic rate (BMR) = 'no activity' mode : ", bmr, "kcal")
+    print("Caloric needs for your lifestyle: ", caloric_needs, "kcal")
