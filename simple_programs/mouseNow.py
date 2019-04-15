65b04e82e87d866b59490d149c9a37cb9efa74bd
Lukasz <lukas.kozicki@gmail.com>
2019-04-15 10:41:34 +0200

    file moved. docstring added

---
+++ simple_programs/mouseNow.py
@@ -0,0 +1,21 @@
+"""
+    This simple program was written for learning and exercise purposes.
+
+    Features involved:
+        - pyautogui
+
+    Sources:
+        https://automatetheboringstuff.com/chapter18/
+"""
+
+import pyautogui
+import time
+
+
+while True:
+    x, y = pyautogui.position()
+    position_str = str(x) + ', ' + str(y)
+    print(position_str, end='')
+    time.sleep(0.01)
+    print('\b' * len(position_str), end='', flush=True)
+
