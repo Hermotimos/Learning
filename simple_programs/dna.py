61059beceab4d0e1a4657a4e38da1dc10c35530c
Lukasz <lukas.kozicki@gmail.com>
2019-04-15 08:46:52 +0200

    docstrings expanded

--- simple_programs/dna.py
+++ simple_programs/dna.py
@@ -10,6 +10,7 @@
         - comprehensions
         - multiple assignment
         - arguments packing/unpacking
+        - docstrings
 
     Sources:
         https://www.flynerd.pl/2018/01/python-metody-typu-string.html
@@ -37,17 +38,45 @@ SAMPLE_1 = "ACTGTGCTGACTCCCGGTGCTGCCGCTGCCATAGCTAAAGCCCGGGTCCTGGTAGGCAGGCGGGAAGC
 
 
 def main():
+    """
+    Ask user for DNA sequence, return number of its occurrences in the sample DNA sequence 'SAMPLE_1'.
+
+    If the searched sequence contains characters not valid as DNA nucleobases, user may decide to modify it by
+    removing invalid characters.
+
+    Returns
+    -------
+    Prints searched sequence (possibly modified), goes to next line, prints number of its occurrences in 'SAMPLE_1'.
+    """
     searched_seq = ask_valid_sequence()
     print(searched_seq, '\n', count_sequence_in_sample(searched_seq, SAMPLE_1))
 
 
 def count_sequence_in_sample(sequence, sample):
-    """ Return count of given DNA sequence/nucleobase occurrences in sample DNA sequence ignoring case. """
+    """
+    Return count of given DNA sequence/nucleobase occurrences in sample DNA sequence ignoring case.
+
+    Parameters
+    ----------
+    sequence (str): Arbitrarily long sequence.
+    sample (str): Arbitrarily long sequence.
+
+    Returns
+    -------
+    int: Count of occurrences of sequence in sample.
+    """
     sequence, sample = sequence.upper(), sample.upper()
     return sample.count(sequence)
 
 
 def ask_valid_sequence():
+    """
+    Ask user for a DNA sequence, check if provided sequence is valid DNA sequence. If not offer options for correction.
+
+    Returns
+    -------
+    str: DNA sequence rid of errors as per user's choice.
+    """
     sequence = input("Please enter sequence to search for in the DNA sample 'SAMPLE_1'\n")
     try:
         assert isdna(sequence)
@@ -75,13 +104,34 @@ def ask_valid_sequence():
 
 
 def isdna(seq):
-    """ Verify if sequence is combined of possible DNA nucleobases. Return bool. """
+    """
+    Verify if sequence is combined of possible DNA nucleobases. Return bool.
+
+    Parameters
+    ----------
+    seq (str): DNA sequence acquired from user in ask_valid_sequence().
+
+    Returns
+    -------
+    bool: True when seq is subset of DNA_NUCLEOBASES (i.e. seq is composed of letters for valid DNA nucleobases).
+          False otherwise.
+    """
     assert isinstance(seq, str), "Please enter data in str format!"
     return set(seq).issubset(DNA_NUCLEOBASES)
 
 
 def get_error_types(seq):
-    """ Print info about erorrs present in errors set. """
+    """
+    Return info about errors present in sequence 'seq'.
+
+    Parameters
+    ----------
+    seq (str): DNA sequence provided by ask_valid_sequence().
+
+    Returns
+    -------
+    str: Information for user about error types found in seq.
+    """
     err1, err2, err3 = False, False, False
     errors_set = find_errors(seq)
     for n in errors_set.copy():             # necessary to avoid: 'RuntimeError: Set changed size during iteration'
@@ -98,16 +148,39 @@ def get_error_types(seq):
             f"3. [{err3}]: Other errors: {', '.join(e for e in errors_set)}")
 
 
-def from_sequence_delete_errors(sequence, *errors):
-    """ Return sequence without given pattern regardless of pattern's upper/lower case. """
-    for err in errors:
-        sequence = sequence.replace(err.lower(), "").replace(err.upper(), "")
-    return sequence
-
-
 def find_errors(seq):
-    """ Return set of letters in sequence that aren't valid DNA nucleabases. """
+    """
+    Return set of letters in seq that aren't valid DNA nucleabases.
+
+    Parameters
+    ----------
+    seq (str): DNA sequence provided by get_error_types().
+
+    Returns
+    -------
+    set: All characters in seq recognised as not beeing valid DNA nucleobases.
+    """
     return {n for n in set(seq) if n not in DNA_NUCLEOBASES}
+
+
+def from_sequence_delete_errors(sequence, *errors):
+    """
+    Return sequence without given pattern regardless of pattern's upper/lower case.
+
+    For each error in *errors iterated over seq and removes err.
+
+    Parameters
+    ----------
+    sequence (str): DNA sequence provided by ask_valid_sequence().
+    *errors: variable length iterable
+
+    Returns
+    -------
+    str: Sequence with specified characters removed.
+    """
+    for err in errors:
+        sequence = sequence.replace(err.lower(), "").replace(err.upper(), "")   # assignment! (replace() returns copy)
+    return sequence
 
 
 main()
