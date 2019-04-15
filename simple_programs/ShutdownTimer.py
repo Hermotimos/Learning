33b59c4be87ba43770060739d5d499275901e3f4
Lukasz <lukas.kozicki@gmail.com>
2019-04-15 11:05:21 +0200

    rearranged order of functions, minor changes

--- simple_programs/ShutdownTimer.py
+++ simple_programs/ShutdownTimer.py
@@ -5,51 +5,11 @@
         - modules import (os, time)
         - user defined functions
         - exception handling, assertions
-        - flow control: if else, while
-
-    Sources:
-        https://automatetheboringstuff.com/chapter18/
+        - flow control: if else, while loop
 """
-
 
 import os
 import time
-
-
-def shutdown_countdown():
-    mode = ask_shutdown_mode()
-    minutes = ask_time()
-    ending = ''
-
-    if minutes == 1:
-        ending = 'ę'
-    elif 1 < minutes < 5:
-        ending = 'y'
-    print(f'\nKomputer zostanie wyłączony za {minutes} minut{ending}.'
-          '\n\t ...Zapisz otwarte pliki przed końcem odliczania, aby uniknąć utraty danych.'
-          '\n\t\t ...W każdej chwili możesz przerwać odliczanie przez wyłączenie okna programu.')
-
-    timer_secs = minutes * 60
-    while timer_secs > 0:
-        """
-        'minutes' are deprecated before first print, 
-        cause exe file created by pyinstaller misses a minute somehow and prints this variable only after 1 min
-        """
-        minutes -= 1
-        print('\n', str(minutes), end='')
-        timer_secs -= 60
-        time.sleep(60)
-    os.system(f"{mode}")
-
-
-def ask_time():
-    entry = input("Podaj za ile minut chcesz wyłączyć komputer i zatwierdź przez ENTER:\n")
-    try:
-        assert int(entry) > 0
-        return int(entry)
-    except Exception:
-        print("Podana wartość jest błędna. Spróbuj jeszcze raz.\n")
-        return ask_time()
 
 
 def ask_shutdown_mode():
@@ -65,6 +25,43 @@ def ask_shutdown_mode():
     except AssertionError:
         print("Podana wartość jest błędna. Spróbuj jeszcze raz.\n")
         return ask_shutdown_mode()
+
+
+def ask_time():
+    entry = input("Podaj za ile minut chcesz wyłączyć komputer i zatwierdź przez ENTER:\n")
+    try:
+        assert int(entry) > 0
+        return entry
+    except Exception:
+        print("Podana wartość jest błędna. Spróbuj jeszcze raz.\n")
+        return ask_time()
+
+
+def shutdown_countdown():
+    mode = ask_shutdown_mode()
+    minutes_str = ask_time()
+    ending = ''
+
+    if minutes_str == "1":
+        ending = 'ę'
+    elif 1 < int(minutes_str[-1]) < 5:
+        ending = 'y'
+    print(f'\nKomputer zostanie wyłączony za {minutes_str} minut{ending}.'
+          '\n\t ...Zapisz otwarte pliki przed końcem odliczania, aby uniknąć utraty danych.'
+          '\n\t\t ...W każdej chwili możesz przerwać odliczanie przez wyłączenie okna programu.')
+
+    minutes_int = int(minutes_str)
+    timer_secs = minutes_int * 60
+    while timer_secs > 0:
+        """
+        'minutes' are deprecated before first print, 
+        cause exe file created by pyinstaller misses a minute somehow and prints this variable only after 1 min
+        """
+        minutes_int -= 1
+        print('\n', str(minutes_int), end='')
+        timer_secs -= 60
+        time.sleep(60)
+    os.system(f"{mode}")
 
 
 shutdown_countdown()
