1152cc490a85cf8b3cb75736683d21cb6209e24f
Hermotimos <lukas.kozicki@gmail.com>
2019-04-18 13:27:34 +0200

    docstring changed

--- algorithms/MIT_guess_root.py
+++ algorithms/MIT_guess_root.py
@@ -2,7 +2,7 @@
     This file is for learning and exercise purposes.
 
     Topics:
-        - algorithms
+        - algorithms: guess, approximation and bisection algorithms
 
     Sources:
         https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
