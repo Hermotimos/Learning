5c4c73b4ea4cbd7e0e0305efebc44750e524148c
Hermotimos <lukas.kozicki@gmail.com>
2019-04-18 20:26:38 +0200

    memoization added

--- algorithms/Fibonacci.py
+++ algorithms/Fibonacci.py
@@ -7,29 +7,12 @@
     Sources:
         https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
         https://www.youtube.com/watch?v=7k7tMKxH6Dg
+        https://www.youtube.com/watch?v=Qk0zUZW-U_M
 """
 
 
 # ----------------------------------------------------------------------------------------------------
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI WITH DICT<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-# ----------------------------------------------------------------------------------------------------
-def fib_with_dict(num):
-    d = {0: 0, 1: 1}        # serves as starting point
-    if num in d:
-        return d[num]
-    else:
-        answer = fib_with_dict(num - 1) + fib_with_dict(num - 2)
-        d[num] = answer
-        return answer
-
-
-print('\nFibonacci with dict MIT')
-for i in range(0, 11):
-    print(fib_with_dict(i), '', end='')
-
-
-# ----------------------------------------------------------------------------------------------------
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI WITH FORESIGHT<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# >>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI WITH ITERATION AND  MULTIPLE ASSIGNMENT<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
 
 def fibonacci(n):
@@ -41,13 +24,13 @@ def fibonacci(n):
 
 
 # This one always keeps track of the next value in sequence, hence 'foresight'
-print('\nFibonacci YT')
+print('\nFibonacci with iteration and multiple assignment [YT]')
 for i in range(0, 11):
     print(fibonacci(i), '', end='')
 
 
 # ----------------------------------------------------------------------------------------------------
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI SIMPLEST<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI WITH RECURSION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
 
 def fib_mit(n):
@@ -57,7 +40,7 @@ def fib_mit(n):
         return fib_mit(n-1) + fib_mit(n-2)
 
 
-print('\nFibonacci simplest MIT')
+print('\nFibonacci with recursion [MIT]')
 for i in range(0, 11):
     print(fib_mit(i), '', end='')
 # Printout shows bug in this algorithm. It's one step too far into the Fibonacci (skipped value for 0).
@@ -73,6 +56,75 @@ def fib_simplest(n):
         return fib_simplest(n-1) + fib_simplest(n-2)
 
 
-print('\nFibonacci simplest MIT without bug')
+print('\nFibonacci with recursion without bug [MIT]')
 for i in range(0, 11):
     print(fib_simplest(i), '', end='')
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FIBONACCI WITH MEMOIZATION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+def fib_with_dict(num):
+    if num in d:
+        return d[num]
+    else:
+        answer = fib_with_dict(num - 1) + fib_with_dict(num - 2)
+        d[num] = answer
+        return answer
+
+
+d = {0: 0, 1: 1}
+print('\nFibonacci with memoization: dict with starting values, serves as cache [MIT]')
+for i in range(0, 111):
+    print(fib_with_dict(i), '', end='')
+
+
+# -----------------------------------------------------------------------------------------------------
+fib_cache = {}
+
+
+def fib_memo(n):
+    if n in fib_cache:
+        return fib_cache[n]
+
+    if n == 0:
+        val = 0
+    elif n == 1:
+        val = 1
+    elif n > 1:
+        val = fib_memo(n-1) + fib_memo(n-2)
+
+    fib_cache[n] = val
+    return val
+
+
+# This one is MUCH more efficient.
+# It prints out range(0, 111) in a second, whereas all the above slow down by 20th.
+print('\nFibonacci with memoization: no starter, build inner data itself [YT]')
+for i in range(0, 111):
+    print(fib_memo(i), '', end='')
+
+
+# Memoization can be done with module functools, decorator @lru_cache() [Last Recently Used]
+import functools
+
+
+@functools.lru_cache(maxsize=1000)
+def fib_decorated(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n > 1:
+        return fib_decorated(n-1) + fib_decorated(n-2)
+
+
+print('\nFibonacci with memoization: with decorator [YT]')
+for i in range(0, 111):
+    print(fib_decorated(i), '', end='')
+
+
+# The golden ratio of Fibonacci sequence
+print('\n\nRatio of increment:')
+for i in range(1, 11):
+    print(fib_decorated(i + 1) / fib_decorated(i))
