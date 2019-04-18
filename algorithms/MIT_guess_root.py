209ab8eb70d580c39d493b415aec6460c4a7973f
Hermotimos <lukas.kozicki@gmail.com>
2019-04-18 11:35:01 +0200

    approximation algorithm added

--- algorithms/MIT_guess_root.py
+++ algorithms/MIT_guess_root.py
@@ -8,8 +8,9 @@
         https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
 """
 
-
-# GUESS-AND-CHECK
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>GUESS-AND-CHECK<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
 def guess1(cube):
     for guess in range(cube + 1):
         if guess**3 == cube:
@@ -42,8 +43,44 @@ guess2(8)
 guess2(-27)
 guess2(-64)
 guess2(100)
+print()
 
 
-# APPROXIMATE SOLUTION:
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>APPROXIMATE SOLUTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+# In MIT source was: while abs(cube - guess ** 3) >= ...
+# With my change program doesn't enter infinite loop for cube = 10000 (that was example of infinite loop in MIT lecture)
+# Also added code for negative cubes.
+def approximate_cube_root(cube, max_inaccuracy=0.0001, increment=0.01):
+    guess = 0.0
+    guess_counter = 0
+
+    while (abs(cube) - guess ** 3) >= max_inaccuracy:
+        guess += increment
+        guess_counter += 1
+    if (abs(cube) - guess ** 3) >= max_inaccuracy:
+        print(f"""Failed on cube root of {cube}.
+                Number of guesses: {guess_counter}.
+                Maximal inaccuracy allowed: {max_inaccuracy}.
+                Increment set to: {increment}.\n""")
+    else:
+        if cube < 0:
+            guess = - guess
+        print(f"""Cube root of {cube} is approximately  {guess}.
+                Number of guesses: {guess_counter}.
+                Maximal inaccuracy allowed: {max_inaccuracy}.
+                Increment set to: {increment}.
+                {guess}**3 = {guess**3}\n""")
 
 
+approximate_cube_root(10000, max_inaccuracy=0.00001, increment=0.0001)
+approximate_cube_root(-10000)
+for num in range(9):
+    approximate_cube_root(num)
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>BISECTION SEARCH<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
