9767626a740deffd463c368a06c12ed32ba1b1fe
Hermotimos <lukas.kozicki@gmail.com>
2019-04-21 20:56:38 +0200

    file created

---
+++ algorithms/6.SEARCH ALGORITHMS.py
@@ -0,0 +1,10 @@
+"""
+    This file is for learning and exercise purposes.
+
+    Topics:
+        - algorithms: linear search, bisection search, todo ..............
+
+    Sources:
+        https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
+"""
+
