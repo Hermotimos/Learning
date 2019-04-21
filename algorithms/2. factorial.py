d3b04236abca8f15ba73b1fb0e0ca0c9913606db
Hermotimos <lukas.kozicki@gmail.com>
2019-04-21 10:16:15 +0200

    Performance tests

--- algorithms/2. factorial.py
+++ algorithms/2. factorial.py
@@ -26,14 +26,14 @@ def factorial_recursive(n):
         return n * factorial_recursive(n - 1)
 
 
+# Shorter but harder to read version:
+def factorial_recursive_short(n): return 1 if n == 0 else n * factorial_recursive(n - 1)
+
+
 print('\nFactorial recursive')
 for val in range(11):
     print(val, '=>', factorial_recursive(val))
 # print(factorial_recursive(12345))  ------------> Recursive algorithm is not efficient enough to compute this.
-
-
-# Shorter but harder to read version is:
-def factorial_recursive_short(n): return 1 if n == 0 else n * factorial_recursive(n - 1)
 
 
 # ----------------------------------------------------------------------------------------------------
@@ -77,7 +77,6 @@ print(factorial_iterative_2(12345))
 # MEMOIZATION: in case of factorials there's no gain in performance if function is called just once.
 # The gain occurs after we call it once to get some data into cache dict 'factorials', and only subsequent calls use it.
 
-
 factorials = {}
 
 
@@ -103,8 +102,11 @@ print(factorial_recur_memo(y))
 print(f'Time for {y}!: ', time.time() - timer)
 print()
 
-# This one uses factorials cache, slight improvement in performance
-print('Factorial recursive with memoizaition - iteration 2')
+# This one already uses factorials cache, so there's slight improvement in performance
+print('\n', '-' * 50)
+print('Factorial recursive with memoizaition - iteration 2 (improvement in performance)')
+print('-' * 50)
+
 timer = time.time()
 y = 3500
 print(factorial_recur_memo(y))
@@ -131,7 +133,6 @@ print(f'Time for {y}!: ', time.time() - timer)
 
 
 # ----------------------------------------------------------------------------------------------------
-
 
 factorials_2 = {}
 
@@ -169,17 +170,6 @@ print(factorial_recur_memo_short(y))
 print(f'Time for {y}!: ', time.time() - timer)
 print()
 
-timer = time.time()
-y = 5000
-print(factorial_recur_memo_short(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
-
-timer = time.time()
-y = 8000
-print(factorial_recur_memo_short(y))
-print(f'Time for {y}!: ', time.time() - timer)
-
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -208,5 +198,36 @@ def time_function(function):
     return wrapper
 
 
-clocked_factorial = time_function(factorial_recur_memo_short)
-clocked_factorial(3000)
+print('PERFORMANCE: iterative 1')
+clocked_factorial = time_function(factorial_iterative_1)
+for v in (3000, 3000, 5000, 5000, 8000, 8000, 20000, 90000, 90000):
+    clocked_factorial(v)
+print()
+
+print('PERFORMANCE: iterative 2')
+clocked_factorial = time_function(factorial_iterative_2)
+for v in (3000, 3000, 5000, 5000, 8000, 8000, 20000, 90000, 90000):
+    clocked_factorial(v)
+print()
+
+
+print('PERFORMANCE: recursive')
+clocked_factorial = time_function(factorial_recursive)
+for v in (3000, 3000):          # THIS WON'T EVEN COMPUTE FOR 5000 !!!!!!
+    clocked_factorial(v)
+print()
+
+
+print('PERFORMANCE: recursive with memoization')
+clocked_factorial = time_function(factorial_recur_memo)
+for v in (3000, 3000, 5000, 5000, 8000, 8000, 11500, 15000, 18500, 22000, 25500, 29000, 32500, 32500):
+    clocked_factorial(v)
+print()
+
+
+# CONCLUSION:
+# ----------
+# RECURSIVE: Without memoization recursive algorithm is a joke.
+# ITERATIVE: performs very well for small numbers (up to 20000). But then it slows down significantly.
+# RECURSIVE WITH MEMOIZATION: works well if it can populate its cache with gradually increasing searched n.
+#                             So it's potentially better than iterative for big numbers, but under conditions.
