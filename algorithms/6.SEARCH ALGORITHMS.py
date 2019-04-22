95d87f1f9140c38fcf1aa2d030a575e07684db78
Hermotimos <lukas.kozicki@gmail.com>
2019-04-22 15:30:07 +0200

    version without printouts from within added (shorter and cleaner)

--- algorithms/6.SEARCH ALGORITHMS.py
+++ algorithms/6.SEARCH ALGORITHMS.py
@@ -25,16 +25,121 @@ print(sorted(UNSORTED_NUMBERS))
 # ----------------------------------------------------------------------------------------------------
 
 
+def linear_search_unsorted(iterable, searched):
+    for e in iterable:
+        if e == searched:
+            return True
+    return False
+
+
+print('LINEAR SEARCH ON UNSORTED ITERABLE')
+print(linear_search_unsorted(UNSORTED_NUMBERS, 4))
+print(linear_search_unsorted(UNSORTED_NUMBERS, 11))
+print(linear_search_unsorted(UNSORTED_NUMBERS, 44))
+print(linear_search_unsorted(UNSORTED_LETTERS, 'z'))
+print(linear_search_unsorted(UNSORTED_LETTERS, 'h'))
+print()
+
+
+# LINEAR SEARCH ON SORTED ITERABLE
+
+
+def linear_search_sorted(iterable, searched):
+    for e in range(len(iterable)):
+        if iterable[e] == searched:
+            return True
+        if iterable[e] > searched:
+            return False
+    return False
+
+
+print('LINEAR SEARCH ON SORTED ITERABLE')
+print(linear_search_sorted(SORTED_NUMBERS, 4))
+print(linear_search_sorted(SORTED_NUMBERS, 44))
+print(linear_search_sorted(SORTED_NUMBERS, 11))
+print(linear_search_sorted(SORTED_LETTERS, 'z'))
+print(linear_search_sorted(SORTED_LETTERS, 'h'))
+print()
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>> BISECTION SEARCH [REQUIRES SORTED ITERABLE] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+
+# 1) ALGORITHM WITH COPYING OF THE LIST => cost O(n)
+# cost of recursion is O(log n) but cost of copying the list is O(n)
+
+
+def bisection_search(iterable, searched):
+    if len(iterable) == 0:
+        return False
+    elif len(iterable) == 1:
+        return iterable[0] == searched
+    else:
+        half = len(iterable) // 2
+        if iterable[half] > searched:
+            return bisection_search(iterable[:half], searched)
+        else:
+            return bisection_search(iterable[half:], searched)
+
+
+print('BISECTION SEARCH - WITH COPYING')
+print(bisection_search(SORTED_NUMBERS, 4))
+print(bisection_search(SORTED_NUMBERS, 44))
+print(bisection_search(SORTED_NUMBERS, 11))
+print(bisection_search(SORTED_LETTERS, 'z'))
+print(bisection_search(SORTED_LETTERS, 'h'))
+print()
+
+
+# 2) MY ALGORITHM => cost O(log n)
+# based on hint from MIT lecture to use pointers instead of copying iterable
+
+
+def bisection_search_efficient_1(iterable, searched, low=0, high=0):
+    if high == 0:
+        high = len(iterable)
+
+    if len(iterable) == 0:
+        return False
+    elif len(iterable[low:high]) == 1:
+        return iterable[0] == searched
+
+    else:
+        middle = (low + high) // 2
+        if iterable[middle] == searched:
+            return True
+        elif iterable[middle] > searched:
+            high = middle
+        else:
+            low = middle
+        return bisection_search_efficient_1(iterable, searched, low, high)
+
+
+print('BISECTION SEARCH - WITH POINTERS')
+print(bisection_search_efficient_1(SORTED_NUMBERS, 4))
+print(bisection_search_efficient_1(SORTED_NUMBERS, 44))
+print(bisection_search_efficient_1(SORTED_NUMBERS, 11))
+print(bisection_search_efficient_1(SORTED_LETTERS, 'z'))
+print(bisection_search_efficient_1(SORTED_LETTERS, 'h'))
+print()
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PERFORMANCE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+print('# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PERFORMANCE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+
 # LINEAR SEARCH ON UNSORTED ITERABLE
-
-
 def linear_search_unsorted(iterable, searched):
     found = False
     steps = 0
     for e in iterable:
         steps += 1
         if e == searched:
-            found = True
+            found = True                # returning True here would speed up, but not in worst case scenario (e absent)
     return f"""Steps: {steps:5}\t{'not ' if found == False else "":}found '{searched}' in {len(iterable)} elements"""
 
 
@@ -54,8 +159,6 @@ print()
 
 
 # LINEAR SEARCH ON SORTED ITERABLE
-
-
 def linear_search_sorted(iterable, searched):
     found = False
     steps = 0
@@ -90,8 +193,6 @@ print()
 
 
 # 1) ALGORITHM WITH COPYING OF THE LIST => cost O(n)
-# cost of recursion is O(log n) but cost of copying the list is O(n)
-
 
 def bisection_search(iterable, searched):
     if len(iterable) == 0:
@@ -121,10 +222,7 @@ print(bisection_search(SORTED_LETTERS_TUPLE, 'h'))
 print()
 
 
-
 # 2) MY ALGORITHM => cost O(log n)
-# based on hint from MIT lecture to use pointers instead of copying iterable
-
 
 def bisection_search_efficient_1(iterable, searched, low=0, high=0):
     if high == 0:
