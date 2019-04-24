fa500e750d06ee884933b845c207f24ec1760fbf
Hermotimos <lukas.kozicki@gmail.com>
2019-04-24 18:09:04 +0200

    2 algorithms for hanoi

--- algorithms/5.Hanoi.py
+++ algorithms/5.Hanoi.py
@@ -1,1 +1,79 @@
-# that's for later
+"""
+    This file is for learning and exercise purposes.
+
+    Topics:
+        - algorithms: recursion
+
+    Sources:
+        https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
+        https://www.dima.to/blog/?p=29
+        https://www.python-course.eu/towers_of_hanoi.php
+"""
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> VERSION 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+
+def hanoi(size, source, target, middle):
+    if size == 1:
+        target.append(source.pop())
+    else:
+        hanoi(size-1, source, middle, target)
+        hanoi(1, source, target, middle)
+        hanoi(size-1, middle, target, source)
+
+
+# VERSION 1 WITH PRINTOUT:
+def hanoi_print(size, source, target, middle):
+    if size == 0:
+        print('\na {}\nb {}\nc {}'.format(*towers))
+    if size == 1:
+        target.append(source.pop())
+        print('\na {}\nb {}\nc {}'.format(*towers))
+    else:
+        hanoi_print(size - 1, source, middle, target)
+        hanoi_print(1, source, target, middle)
+        hanoi_print(size - 1, middle, target, source)
+
+
+print('------------ VERSION 1 ------------')
+a, b, c = [*range(2, 0, -1)], [], []
+towers = a, b, c
+print('START:\na {}\nb {}\nc {}'.format(*towers))
+
+hanoi_print(len(a), a, c, b)
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> VERSION 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+
+def hanoi2(size, source, middle, target):
+    if size == 0:
+        return
+    hanoi2(size - 1, source, target, middle)
+    if source:
+        target.append(source.pop())
+    hanoi2(size - 1, middle, source, target)
+
+
+# VERSION 2 WITH PRINTOUT:
+def hanoi2_print(size, source, middle, target):
+    if size == 0:
+        return
+    hanoi2_print(size - 1, source, target, middle)
+    if source:
+        target.append(source.pop())
+    print('\na {}\nb {}\nc {}'.format(*towers))
+    hanoi2_print(size - 1, middle, source, target)
+
+
+print('------------ VERSION 2 ------------')
+a, b, c = [*range(4, 0, -1)], [], []
+towers = a, b, c
+print('START:\na {}\nb {}\nc {}'.format(*towers))
+
+hanoi2_print(len(a), a, b, c)
