ee06f389a9ac24814df13e6a08041f591621a743
Hermotimos <lukas.kozicki@gmail.com>
2019-04-24 22:15:52 +0200

    comments and minor changes

--- algorithms/8.SORT ALGORITHMS.py
+++ algorithms/8.SORT ALGORITHMS.py
@@ -23,6 +23,7 @@ list1 = [1, 2, 3, 4, 4]
 tuple2 = 1, 2, 3, 4, 3
 list2 = [1, 2, 3, 4, 3, 4, 3]
 
+print('Test is_sorted(): True, True, False, False')
 print(is_sorted(tuple1))
 print(is_sorted(list1))
 print(is_sorted(tuple2))
@@ -38,14 +39,16 @@ def bogo_sort(iterable, shuffle_counter=0):
     print(f'Steps needed: {shuffle_counter}: {iterable}')
 
 
-tuple2 = 1, 2, 3, 4, 3
-list2 = [1, 2, 3, 4, 3, 4, 3]
-# bogo_sort(tuple2)             # Tuple is immutable: TypeError: 'tuple' object does not support item assignment
-bogo_sort(list2)
+print('MONKEY SEARCH / BOGO SEARCH / SHOTGUN SEARCH')
+list1 = [1, 2, 3, 1, 4, 3, 4, 3, 1]
+list2 = [(2, 'two'), (3, 'three'), (0, 'zero'), (4, 'four'), (1, 'one')]
+list3 = [17, 2, 73, 1, 34, 3, 54, 43, 11, 1, 2, 3, 1, 4, 3, 4, 3, 1, 17, 2, 73, 1, 34, 3, 54, 43, 11, 1, 2, 3, 1, 4, 3]
+print('\nlist1'), bogo_sort(list1)
+print('\nlist2'), bogo_sort(list2)
+# print('\nlist3'), bogo_sort(list3)        # Woooops... this probably takes hours.
 print()
 # This algorithm will eventually sort short lists.
-# For 7-element iterable 'list2' steps needed in 10 trials: 327, 325, 235, 204, 169, 421, 401, 593, 3, 391
-# LOL 3 steps - that's goooood. But sometimes it takes up to 900+, and once it needed 2400 steps...
+# 10 trials for a 7-element iterable: 327, 325, 235, 204, 169, 421, 401, 593, 3, 391. Generally seen range 3 - 2400 !!!
 # Order of growth O is here unbounded in the worst case scenario.
 
 
@@ -66,8 +69,8 @@ def bubble_sort(iterable):
 
 
 # VERSION WITH PRINTOUT:
-def bubble_sort2(iterable):
-    print(f'\nStart: {iterable}')
+def bubble_sort2(iterable, printout=True):
+    print(f'Start: {iterable}') if printout else None
     step_count = 0
     while True:
         swap = True
@@ -76,19 +79,25 @@ def bubble_sort2(iterable):
                 swap = False
                 step_count += 1
                 iterable[e - 1], iterable[e] = iterable[e], iterable[e - 1]
-                print(f'Step {step_count}: {iterable}')
+                print(f'Step {step_count}: {iterable}') if printout else None
         if swap:
-            return iterable
+            return iterable if printout else print(f'Steps: {step_count}: {iterable}.')
 
 
+print('BUBBLE SEARCH')
 list1 = [1, 2, 3, 1, 4, 3, 4, 3, 1]
 list2 = [(2, 'two'), (3, 'three'), (0, 'zero'), (4, 'four'), (1, 'one')]
-print(bubble_sort2(list1))
-print(bubble_sort2(list2))
+list3 = [17, 2, 73, 1, 34, 3, 54, 43, 11, 1, 2, 3, 1, 4, 3, 4, 3, 1, 17, 2, 73, 1, 34, 3, 54, 43, 11, 1, 2, 3, 1, 4, 3]
+print('\nlist1'), bubble_sort2(list1)
+print('\nlist2'), bubble_sort2(list2)
+print('\nlist3'), bubble_sort2(list3, False)
+
+# Order of growth O(n**2) because each iterative solution is O(len(iterable) = O(n).
+# There are 2 iterative constructs (while and for), so O(n)*O(n) = O(n**2)
 
 
 # ----------------------------------------------------------------------------------------------------
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> BUBBLE SORT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SELECTION SORT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
 
 
