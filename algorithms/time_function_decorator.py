44b25c788a182dd2357cd641fea6c5bf434015ab
Hermotimos <lukas.kozicki@gmail.com>
2019-04-25 17:34:43 +0200

    dosctring and type of timer changed

--- algorithms/time_function_decorator.py
+++ algorithms/time_function_decorator.py
@@ -2,9 +2,16 @@ import time
 
 
 def time_function(function):
+    """Returns function execution time.
+
+    Possible changes for different measured units:
+    - for seconds:              time.time()
+    - for nanoseconds:          time.perf_counter_ns()
+    - for fractional seconds:   time.perf_counter
+    """
     def wrapper(*args):
-        timer_start = time.time()
+        timer_start = time.perf_counter_ns()
         function(*args)
-        timer_stop = time.time() - timer_start
-        print('Time elapsed for {}({}): {}'.format(function.__name__, ",".join(str(a) for a in args), timer_stop))
+        timer_stop = time.perf_counter_ns() - timer_start
+        print('Time for {:25}: {}'.format(function.__name__, timer_stop))
     return wrapper
