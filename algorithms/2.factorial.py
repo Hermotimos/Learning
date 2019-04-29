c270b3cb7f3a22b1c512fc30561ab27722daf301
Hermotimos <lukas.kozicki@gmail.com>
2019-04-29 22:37:45 +0200

    formatting for readability

--- algorithms/2.factorial.py
+++ algorithms/2.factorial.py
@@ -9,7 +9,6 @@
     Sources:
         https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/
 """
-import time
 import sys
 from time_function_decorator import time_function
 sys.setrecursionlimit(100000)
@@ -20,27 +19,27 @@ sys.setrecursionlimit(100000)
 # ----------------------------------------------------------------------------------------------------
 
 
-def factorial_recursive(n):
+def fact_recur1(n):
     if n == 0:
         return 1
     else:
-        return n * factorial_recursive(n - 1)
+        return n * fact_recur1(n - 1)
 
 
 # Shorter:
-def factorial_rec_short1(n):
+def fact_recur11(n):
     if n < 2:
         return 1
-    return n * factorial_rec_short1(n - 1)
+    return n * fact_recur11(n - 1)
 
 
-# Still shorter but harder to read:
-def factorial_rec_short2(n): return 1 if n < 2 else n * factorial_rec_short2(n - 1)
+# Shortest but unreadable:
+def fact_recur111(n): return 1 if n < 2 else n * fact_recur111(n - 1)
 
 
 print('\nFactorial recursive')
 for val in range(11):
-    print(val, '=>', factorial_recursive(val))
+    print(val, '=>', fact_recur1(val))
 # print(factorial_recursive(12345))  ------------> Recursive algorithm is not efficient enough to compute this.
 
 
@@ -49,7 +48,7 @@ for val in range(11):
 # ----------------------------------------------------------------------------------------------------
 
 
-def factorial_iterative_1(n):
+def fact_iter1(n):
     product = 1
     for i in range(1, n + 1):
         product *= i
@@ -58,14 +57,14 @@ def factorial_iterative_1(n):
 
 print('\nFactorial iterative 1')
 for val in range(11):
-    print(val, '=>', factorial_iterative_1(val))
-print(factorial_iterative_1(12345))
+    print(val, '=>', fact_iter1(val))
+print(fact_iter1(12345))
 
 
 # ----------------------------------------------------------------------------------------------------
 
 
-def factorial_iterative_2(n):
+def fact_iter2(n):
     product = 1
     while n > 0:
         product *= n
@@ -75,69 +74,40 @@ def factorial_iterative_2(n):
 
 print('\nFactorial iterative 2')
 for val in range(11):
-    print(val, '=>', factorial_iterative_2(val))
-print(factorial_iterative_2(12345))
+    print(val, '=>', fact_iter2(val))
+print(fact_iter2(12345))
 
 
 # ----------------------------------------------------------------------------------------------------
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FACTORIAL WITH MEMOIZATION <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
 # MEMOIZATION: in case of factorials_1 there's no gain in performance if function is called just once.
-# The gain occurs by second call (after data from first is cached in dict 'factorials_1' subsequent calls can use it).
+# The gain occurs by second call (after data from first is cached in dict 'factorials_1', subsequent calls can use it).
 
 factorials_1 = {}
 
 
-def factorial_recur_memo(n):
+def fact_recur_memo1(n):
     if n in factorials_1:
         return factorials_1[n]
     else:
         if n == 0:
             result = 1
         else:
-            result = n * factorial_recur_memo(n - 1)
+            result = n * fact_recur_memo1(n - 1)
         factorials_1[n] = result
         return result
 
 
-print('\n', '-' * 50)
-print('Factorial recursive with memoizaition - iteration 1')
-print('-' * 50)
-
-timer = time.time()
-y = 3500
-print(factorial_recur_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
-
-# This one already uses factorials_1 cache, so there's slight improvement in performance
-print('\n', '-' * 50)
-print('Factorial recursive with memoizaition - iteration 2 (improvement in performance)')
-print('-' * 50)
-
-timer = time.time()
-y = 3500
-print(factorial_recur_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
-
-# Starting from ca. 4000 an more it would throw error if cache 'factorials_1' were empty, now it computes.
-timer = time.time()
-y = 5000
-print(factorial_recur_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
-
+print('\nFactorial recursive with memoization 1')
+time_function(fact_recur_memo1)(3500)
+# The one below already uses factorials_1 cache, so there's slight improvement in performance
+time_function(fact_recur_memo1)(3500)
+# Starting from ca. 4000 and more it would throw error if cache 'factorials_1' were empty, now it computes.
 # Now it's possible to gradually increase numbers, as 'factorials_1' cache gradually gets bigger.
-timer = time.time()
-y = 8000
-print(factorial_recur_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
-
-timer = time.time()
-y = 8000
-print(factorial_recur_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
+time_function(fact_recur_memo1)(5000)
+time_function(fact_recur_memo1)(8000)
+time_function(fact_recur_memo1)(8000)
 
 
 # ----------------------------------------------------------------------------------------------------
@@ -145,110 +115,119 @@ print(f'Time for {y}!: ', time.time() - timer)
 factorials_2 = {}
 
 
-def factorial_recur_memo_short(n):
+def fact_recur_memo2(n):
     if n not in factorials_2:
         if n == 0:
             value = 1
         else:
-            value = n * factorial_recur_memo_short(n - 1)
+            value = n * fact_recur_memo2(n - 1)
         factorials_2[n] = value
     return factorials_2[n]
 
 
-def factorial_recur_memo_shortest(n):
-    if n not in factorials_2:
-        value = 1 if n == 0 else n * factorial_recur_memo_short(n - 1)
-        factorials_2[n] = value
-    return factorials_2[n]
+print('\nFactorial recursive with memoization - 2')
+time_function(fact_recur_memo2)(3500)
+time_function(fact_recur_memo2)(3500)
+time_function(fact_recur_memo2)(5000)
+time_function(fact_recur_memo2)(8000)
+time_function(fact_recur_memo2)(8000)
 
-
-print('\n', '-' * 50)
-print('Factorial recursive with memoization - more concise code')
-print('-' * 50)
-
-timer = time.time()
-y = 3500
-print(factorial_recur_memo_short(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
-
-timer = time.time()
-y = 3500
-print(factorial_recur_memo_short(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
 
 # ----------------------------------------------------------------------------------------------------
 
 factorials_3 = {}
 
 
-def factorial_iter_memo(n):
+def fact_recur_memo3(n):
     if n not in factorials_3:
-        value = 1
-        for n in range(1, n + 1):
-            value *= n
-            factorials_3[n] = value
+        value = 1 if n == 0 else n * fact_recur_memo2(n - 1)
+        factorials_3[n] = value
     return factorials_3[n]
 
 
-print('\n', '-' * 50)
-print('Factorial iterative with memoization')
-print('-' * 50)
+print('\nFactorial recursive with memoization - 3')
+time_function(fact_recur_memo3)(3500)
+time_function(fact_recur_memo3)(3500)
+time_function(fact_recur_memo3)(5000)
+time_function(fact_recur_memo3)(8000)
+time_function(fact_recur_memo3)(8000)
+# This algorithm seems to be a variation of previous one, but it performs better, maybe due to one assignment less.
 
-timer = time.time()
-y = 50000
-print(factorial_iter_memo(y))
-print(f'Time for {y}!: ', time.time() - timer)
-print()
+# ----------------------------------------------------------------------------------------------------
 
-factorials_3 = {}
-print(f'Time for 50000!: ')
+factorials_4 = {}
+
+
+def factorial_iter_memo(n):
+    if n not in factorials_4:
+        value = 1
+        for n in range(1, n + 1):
+            value *= n
+            factorials_4[n] = value
+    return factorials_4[n]
+
+
+print('\nFactorial iterative with memoization')
+time_function(factorial_iter_memo)(3500)
+time_function(factorial_iter_memo)(3500)
+time_function(factorial_iter_memo)(5000)
+time_function(factorial_iter_memo)(8000)
+time_function(factorial_iter_memo)(8000)
+
+print('Cache RESET')
+factorials_4.clear()
 time_function(factorial_iter_memo)(50000)
-# TODO works up to 50000 (1.5 secs) but more will hang the system ==> try out on stronger system.
+
+# TODO works up to 50000! (ca. 1.6 secs) but higher values will hang the system ==> try out on stronger system.
 # TODO: Probably this is caused by searching for memoized/cached data => try with bisection search algorithms
-print()
+
 
 # ----------------------------------------------------------------------------------------------------
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PERFORMANCE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
-factorials_1 = {}
-factorials_2 = {}
-factorials_3 = {}
-# Redeclared to reset stored data before performance test.
 
-print('PERFORMANCE: iterative 1')
-clocked_factorial = time_function(factorial_iterative_1)
+
+print('\n--------------------------------------------------------')
+# Reset cached/memoized data before performance test:
+factorials_1.clear()
+factorials_2.clear()
+factorials_3.clear()
+factorials_4.clear()
+
+
+print('\nPERFORMANCE: iterative 1')
+clocked_factorial = time_function(fact_iter1)
 for v in (3000, 3000, 5000, 5000, 8000, 8000, 20000, 90000, 90000):
-    print(v, '', end=''), clocked_factorial(v)
-print()
+    clocked_factorial(v)
 
-print('PERFORMANCE: iterative 2')
-clocked_factorial = time_function(factorial_iterative_2)
+print('\nPERFORMANCE: iterative 2')
+clocked_factorial = time_function(fact_iter2)
 for v in (3000, 3000, 5000, 5000, 8000, 8000, 20000, 90000, 90000):
-    print(v, '', end=''), clocked_factorial(v)
-print()
+    clocked_factorial(v)
 
 
-print('PERFORMANCE: recursive')
-clocked_factorial = time_function(factorial_recursive)
-for v in (3000, 3000):          # THIS WON'T EVEN COMPUTE FOR 5000 !!!!!!
-    print(v, '', end=''), clocked_factorial(v)
-print()
+print('\nPERFORMANCE: recursive')
+clocked_factorial = time_function(fact_recur1)
+for v in (3000, 3000):
+    clocked_factorial(v)
+print('This algorithm can\'t do 5000!: Process finished with exit code -1073741571 (0xC00000FD)')
 
-
-print('PERFORMANCE: recursive with memoization')
-clocked_factorial = time_function(factorial_recur_memo)
+print('\nPERFORMANCE: recursive with memoization')
+clocked_factorial = time_function(fact_recur_memo1)
 for v in (3000, 3000, 5000, 5000, 8000, 8000, 11500, 15000, 18500, 22000, 25500, 29000, 32500, 32500):
-    print(v, '', end=''), clocked_factorial(v)
-print()
+    clocked_factorial(v)
 
 
-print('PERFORMANCE: iterative with memoization')
+print('\nPERFORMANCE: iterative with memoization')
 clocked_factorial = time_function(factorial_iter_memo)
-for v in (3000, 3000, 5000, 5000, 8000, 8000, 11500, 15000, 18500, 22000, 25500, 29000, 32500, 32500, 50000, 60000):
-    print(v, '', end=''), clocked_factorial(v)
-print()
+for v in (3000, 3000, 5000, 5000, 8000, 8000, 11500, 15000, 18500, 22000, 25500, 29000, 32500, 32500):
+    clocked_factorial(v)
+
+print('\nPERFORMANCE: iterative with memoization can do even more [from scratch, cached data reset]:')
+factorials_4.clear()
+clocked_factorial = time_function(factorial_iter_memo)
+for v in (50000, 60000):
+    clocked_factorial(v)
 
 
 # CONCLUSION:
