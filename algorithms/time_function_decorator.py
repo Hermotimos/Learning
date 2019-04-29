41021ccb4bd282e3e8dc36446281554f92b41f64
Hermotimos <lukas.kozicki@gmail.com>
2019-04-29 09:04:06 +0200

    conversion from scientific notation of numbers to normal

--- algorithms/time_function_decorator.py
+++ algorithms/time_function_decorator.py
@@ -10,10 +10,11 @@ def time_function(function):
     - for fractional seconds:   time.perf_counter
 
     Conversion from nanoseconds to seconds: * 0.000000001
+    Conversion from scientific notation (ex. 2.14e-04) to float with precision 10: format() with {:.10f}
     """
     def wrapper(*args):
         timer_start = time.perf_counter_ns()
         function(*args)
         timer_stop = time.perf_counter_ns() - timer_start
-        print('Time for {:25}: {} secs'.format(function.__name__, round(timer_stop * 0.000000001, 7)))
+        print('Time for {:25}: {:.10f} secs'.format(function.__name__, timer_stop * 0.000000001))
     return wrapper
