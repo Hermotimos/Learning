5a4af3f95030786ee5d1aa51d24c911d3126a9a1
Lukasz <lukas.kozicki@gmail.com>
2019-05-12 15:24:40 +0200

    order and formatting

--- algorithms/7.SEARCH ALGORITHMS.py
+++ algorithms/7.SEARCH ALGORITHMS.py
@@ -13,12 +13,6 @@ UNSORTED_LETTERS = ['c', 'a', 'd', 'b', 'g', 'j', 'k', 'o', 'p', 'q', 'r', 's', 
 SORTED_LETTERS = ['a', 'b', 'c', 'd', 'g', 'j', 'k', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
 UNSORTED_NUMBERS = [4, 5, 6, 7, 1, 2, 3, 8, 9, 10, 13, 15, 16, 18, 31]
 SORTED_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 16, 18, 31]
-
-UNSORTED_LETTERS_TUPLE = ('c', 'a', 'd', 'b', 'g', 'j', 'k', 'o', 'p', 'q', 'r', 's', 'z', 't', 'u', 'v', 'x', 'y')
-SORTED_LETTERS_TUPLE = ('a', 'b', 'c', 'd', 'g', 'j', 'k', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z')
-UNSORTED_NUMBERS_TUPLE = (4, 5, 6, 7, 1, 2, 3, 8, 9, 10, 13, 15, 16, 18, 31)
-SORTED_NUMBERS_TUPLE = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 16, 18, 31)
-
 print(sorted(UNSORTED_NUMBERS))
 
 # ----------------------------------------------------------------------------------------------------
@@ -68,7 +62,35 @@ print()
 # ----------------------------------------------------------------------------------------------------
 
 
-# 1) ALGORITHM WITH COPYING OF THE LIST => cost O(n)
+# 1) Aditya Bhardava - iterative bisection
+# there was no division //2 in the book - must be a mistake, cause without it it's not a bisection
+# Cost must be O(log n) because there's no copying of the iterable - it's like pointers.
+
+def bisection_iter(iterable, elem):
+    low = 0
+    high = len(iterable) - 1
+
+    while low <= high:
+        guess = (low + high) // 2
+        if iterable[guess] == elem:
+            return guess
+        if iterable[guess] > elem:
+            high = guess - 1
+        else:
+            low = guess + 1
+    return False
+
+
+print('BISECTION SEARCH ITERATIVE - Aditya Bhargava - Algorytmy. Ilustrowany przewodnik')
+print(bisection_iter(SORTED_NUMBERS, 4))
+print(bisection_iter(SORTED_NUMBERS, 44))
+print(bisection_iter(SORTED_NUMBERS, 11))
+print(bisection_iter(SORTED_LETTERS, 'z'))
+print(bisection_iter(SORTED_LETTERS, 'h'))
+print()
+
+
+# 2) ALGORITHM WITH COPYING OF THE LIST => cost O(n)
 # cost of recursion is O(log n) but cost of copying the list is O(n)
 
 
@@ -107,7 +129,7 @@ print(bisection_search(SORTED_LETTERS, 'h'))
 print()
 
 
-# 2) ALGORITHM WITHOUT COPYING OF THE LIST => cost O(log n)
+# 3) ALGORITHM WITHOUT COPYING OF THE LIST => cost O(log n)
 # uses pointers instead of copying iterable
 # MY OWN ALGORITHM - SIMPLER THAN THE ONE FROM MIT LECTURE
 
@@ -140,33 +162,6 @@ print(bisection_search_efficient_1(SORTED_LETTERS, 'h'))
 print()
 
 
-# 3) Aditya Bhardava - iterative bisection
-# there was no division //2 in the book - must be a mistake, cause without it it's not a bisection
-# Cost must be O(log n) because there's no copying of the iterable - it's like pointers.
-
-def bisection_search3(iterable, elem):
-    low = 0
-    high = len(iterable) - 1
-
-    while low <= high:
-        guess = (low + high) // 2
-        if iterable[guess] == elem:
-            return guess
-        if iterable[guess] > elem:
-            high = guess - 1
-        else:
-            low = guess + 1
-    return False
-
-
-print('BISECTION SEARCH ITERATIVE - Aditya Bhargava - Algorytmy. Ilustrowany przewodnik')
-print(bisection_search3(SORTED_NUMBERS, 4))
-print(bisection_search3(SORTED_NUMBERS, 44))
-print(bisection_search3(SORTED_NUMBERS, 11))
-print(bisection_search3(SORTED_LETTERS, 'z'))
-print(bisection_search3(SORTED_LETTERS, 'h'))
-print()
-
 # ----------------------------------------------------------------------------------------------------
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PERFORMANCE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # ----------------------------------------------------------------------------------------------------
@@ -191,12 +186,6 @@ print(linear_search_unsorted(UNSORTED_NUMBERS, 44))
 print(linear_search_unsorted(UNSORTED_LETTERS, 'z'))
 print(linear_search_unsorted(UNSORTED_LETTERS, 'h'))
 print()
-print(linear_search_unsorted(UNSORTED_NUMBERS_TUPLE, 4))
-print(linear_search_unsorted(UNSORTED_NUMBERS_TUPLE, 44))
-print(linear_search_unsorted(UNSORTED_NUMBERS_TUPLE, 11))
-print(linear_search_unsorted(UNSORTED_LETTERS_TUPLE, 'z'))
-print(linear_search_unsorted(UNSORTED_LETTERS_TUPLE, 'h'))
-print()
 
 
 # LINEAR SEARCH ON SORTED ITERABLE
@@ -220,12 +209,6 @@ print(linear_search_sorted(SORTED_NUMBERS, 11))
 print(linear_search_sorted(SORTED_LETTERS, 'z'))
 print(linear_search_sorted(SORTED_LETTERS, 'h'))
 print()
-print(linear_search_sorted(SORTED_NUMBERS_TUPLE, 4))
-print(linear_search_sorted(SORTED_NUMBERS_TUPLE, 44))
-print(linear_search_sorted(SORTED_NUMBERS_TUPLE, 11))
-print(linear_search_sorted(SORTED_LETTERS_TUPLE, 'z'))
-print(linear_search_sorted(SORTED_LETTERS_TUPLE, 'h'))
-print()
 
 
 # ----------------------------------------------------------------------------------------------------
@@ -233,7 +216,37 @@ print()
 # ----------------------------------------------------------------------------------------------------
 
 
-# 1) ALGORITHM WITH COPYING OF THE LIST => cost O(n)
+# 1) ITERATIVE BINARY SEARCH
+
+def bisection_iter(iterable, searched):
+    low = 0
+    high = len(iterable) - 1
+    steps = 0
+    found = False
+
+    while low <= high:
+        steps += 1
+        guess = (low + high) // 2
+        if iterable[guess] == searched:
+            found = True
+            return f"Steps: {steps:5}\t{'not ' if found == False else '':}found '{searched}' in {len(iterable)} elements"
+        if iterable[guess] > searched:
+            high = guess - 1
+        else:
+            low = guess + 1
+    return f"Steps: {steps:5}\t{'not ' if found == False else '':}found '{searched}' in {len(iterable)} elements"
+
+
+print('BISECTION SEARCH ITERATIVE - Aditya Bhargava - Algorytmy. Ilustrowany przewodnik')
+print(bisection_iter(SORTED_NUMBERS, 4))
+print(bisection_iter(SORTED_NUMBERS, 44))
+print(bisection_iter(SORTED_NUMBERS, 11))
+print(bisection_iter(SORTED_LETTERS, 'z'))
+print(bisection_iter(SORTED_LETTERS, 'h'))
+print()
+
+
+# 2) RECURSIVE ALGORITHM WITH COPYING OF THE LIST => cost O(n)
 
 def bisection_search(iterable, searched):
     if len(iterable) == 0:
@@ -249,21 +262,16 @@ def bisection_search(iterable, searched):
 
 
 print('BISECTION SEARCH - WITH COPYING')
+print('No steps printout by recursion')
 print(bisection_search(SORTED_NUMBERS, 4))
 print(bisection_search(SORTED_NUMBERS, 44))
 print(bisection_search(SORTED_NUMBERS, 11))
 print(bisection_search(SORTED_LETTERS, 'z'))
 print(bisection_search(SORTED_LETTERS, 'h'))
 print()
-print(bisection_search(SORTED_NUMBERS_TUPLE, 4))
-print(bisection_search(SORTED_NUMBERS_TUPLE, 44))
-print(bisection_search(SORTED_NUMBERS_TUPLE, 11))
-print(bisection_search(SORTED_LETTERS_TUPLE, 'z'))
-print(bisection_search(SORTED_LETTERS_TUPLE, 'h'))
-print()
 
 
-# 2) MY ALGORITHM => cost O(log n)
+# 2) MY RECURSIVE ALGORITHM WITHOUT COPYING => cost O(log n)
 
 def bisection_search_efficient_1(iterable, searched, low=0, high=0):
     if high == 0:
@@ -286,15 +294,10 @@ def bisection_search_efficient_1(iterable, searched, low=0, high=0):
 
 
 print('BISECTION SEARCH - WITH POINTERS')
+print('No steps printout by recursion')
 print(bisection_search_efficient_1(SORTED_NUMBERS, 4))
 print(bisection_search_efficient_1(SORTED_NUMBERS, 44))
 print(bisection_search_efficient_1(SORTED_NUMBERS, 11))
 print(bisection_search_efficient_1(SORTED_LETTERS, 'z'))
 print(bisection_search_efficient_1(SORTED_LETTERS, 'h'))
-print()
-print(bisection_search_efficient_1(SORTED_NUMBERS_TUPLE, 4))
-print(bisection_search_efficient_1(SORTED_NUMBERS_TUPLE, 44))
-print(bisection_search_efficient_1(SORTED_NUMBERS_TUPLE, 11))
-print(bisection_search_efficient_1(SORTED_LETTERS_TUPLE, 'z'))
-print(bisection_search_efficient_1(SORTED_LETTERS_TUPLE, 'h'))
 print()
