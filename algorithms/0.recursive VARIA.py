9e221c903d6fa02010b72c9e6445fa853684c346
Hermotimos <lukas.kozicki@gmail.com>
2019-05-21 19:12:01 +0200

    recursive VARIA created with recursive sum, count and max for subscriptable data types

---
+++ algorithms/0.recursive VARIA.py
@@ -0,0 +1,73 @@
+"""
+    This file is for learning and exercise purposes.
+
+    Topics:
+        - algorithms: recursion, monkey sort, bubble sort, selection sort, merge sort
+
+    Sources:
+        Aditya Bhargava - Algorytmy. Ilustrowany przewodnik.
+"""
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RECURSIVE SUM OF ELEMENTS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+def recursive_adding(iterable):
+    if not iterable:
+        return 0
+    else:
+        return iterable[0] + recursive_adding(iterable[1:])
+
+
+print('\n', '\bRECURSIVE SUM OF ELEMENTS')
+list_1 = [1, 2, 3, 4, 10, 11, 12]
+tuple_1 = (1, 2, 3, 4, 10, 11, 12)
+set_1 = {1, 2, 3, 4, 10, 11, 12}      # TypeError: 'set' object is not subscriptable
+
+print(recursive_adding(list_1))
+print(recursive_adding(tuple_1))
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RECURSIVE COUNT OF ELEMENTS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+def recursive_count(iterable):
+    if not iterable:
+        return 0
+    else:
+        return 1 + recursive_count(iterable[1:])
+
+
+print('\n', '\bRECURSIVE COUNT OF ELEMENTS')
+list_1 = [1, 2, 3, 4, 10, 11, 12]
+tuple_1 = (1, 2, 3, 4, 10, 11, 12)
+
+print(recursive_count(list_1))
+print(recursive_count(tuple_1))
+
+
+# ----------------------------------------------------------------------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RECURSIVE MAX ELEMENT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# ----------------------------------------------------------------------------------------------------
+
+
+def recursive_max(iterable):
+    if not iterable:
+        return None
+    elif len(iterable) == 1:
+        return iterable[0]
+    else:
+        mid = len(iterable) // 2
+        half1 = recursive_max(iterable[:mid])
+        half2 = recursive_max(iterable[mid:])
+        return half1 if half1 > half2 else half2
+
+
+print('\n', '\bRECURSIVE MAX ELEMENT')
+list_1 = [1, 2, 3, 4, 10, 11, 12]
+tuple_1 = (1, 2, 3, 4, 10, 11, 12)
+
+print(recursive_max(list_1))
+print(recursive_max(tuple_1))
